2b8175f08be9ea78fbc15d3ba5269536db19988c
masoud <masoud30376@gmail.com>
2023-05-03 10:00:13 +0430

    add submit button in reset page

--- src/pages/reset.jsx
+++ src/pages/reset.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import PageContainer from "../layout/pageContainer.layout";
 import Input from "../components/base/Input";
+import UnitButton from "../components/base/UnitButton";
 
 export const Reset = () => {
 	return (
-		<PageContainer className="bg-[#f6f6f6]  ">
-			<div className="min-h-[100vh] w-[90%]  m-auto">
+		<PageContainer className="bg-[#f6f6f6] relative ">
+			<form className="min-h-[100vh] w-[90%] m-auto">
 				<h1 className="pt-[20vh] pb-[10vh] font-bold text-2xl">
 					تنظیم رمز عبور
 				</h1>
-				<form className="bg-white rounded-sm p-5">
+				<div className="bg-white rounded-sm p-5">
 					<Input placeholder={"رمز عبور"} type={"password"} />
 					<Input placeholder={"تکرار رمز عبور"} type={"text"} />
-				</form>
-			</div>
+				</div>
+				<UnitButton className="bg-primary text-white absolute bottom-6 w-[40%] transform left-1/2 -translate-x-1/2">
+					gis تایید
+				</UnitButton>
+			</form>
 		</PageContainer>
 	);
 };
