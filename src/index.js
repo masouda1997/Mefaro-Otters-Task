a6efba0ef1e01ac26fbd3a164bf2bd793cc61d79
masoud <masoud30376@gmail.com>
2023-05-04 10:20:08 +0430

    install redux toolkit and setup the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store/index";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
+	// <React.StrictMode>
+	<Provider store={store}>
 		<App />
-	</React.StrictMode>
+	</Provider>
+	// </React.StrictMode>
 );
---
+++ src/store/index.jsx
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import themeSlice from "../features/themeSlice";
+
+export default configureStore({
+	reducer: {
+		// themesSlice
+	},
+});
